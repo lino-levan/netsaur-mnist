aaca51551fde7fba794725ccd1551620572eb825
CarrotzRule123 <jwtan02@gmail.com>
2023-05-15 01:13:24 +0800

    implement network

--- islands/Canvas.tsx
+++ islands/Canvas.tsx
@@ -1,17 +1,49 @@
 import { useEffect, useState } from "preact/hooks";
 import IconBallpen from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/ballpen.tsx";
 import IconEraser from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/eraser.tsx";
-import IconSend from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/send.tsx";
+import {
+  setupBackend,
+  WASM,
+  Sequential,
+  tensor,
+  Rank,
+  Tensor,
+} from "https://raw.githubusercontent.com/denosaurs/netsaur/dev/web.ts";
 
 export default function Canvas() {
   const [draw, setDraw] = useState(false);
   const [mode, setMode] = useState("draw");
   const [size, setSize] = useState(2);
   const [prediction, setPrediction] = useState(0);
-  const [strength, setStrength] = useState(0.05);
+  const [predicting, setPredicting] = useState(false);
+  const [strength, setStrength] = useState(0.08);
   const [pixels, setPixels] = useState<Float32Array>(
-    new Float32Array(784).map(() => 1),
+    new Float32Array(784).map(() => 1)
   );
+  const [network, setNetwork] = useState<Sequential | undefined>(undefined);
+
+  function argmax(mat: Tensor<Rank>) {
+    let max = -Infinity;
+    let index = -1;
+    for (let i = 0; i < mat.data.length; i++) {
+      if (mat.data[i] > max) {
+        max = mat.data[i];
+        index = i;
+      }
+    }
+    return index;
+  }
+
+  function normalize(mat: Tensor<Rank>) {
+    const mean = mat.data.reduce((a, k) => a + (1 - k), 0) / 784;
+    const sd = Math.sqrt(
+      mat.data.reduce((a, k) => a + (1 - k) * (1 - k), 0) / 784 - mean * mean
+    );
+    return new Tensor(
+      mat.data.map((k) => ((1 - k) - mean) / sd),
+      mat.shape
+    );
+  }
 
   useEffect(() => {
     const canvas = document.getElementById("canvas") as HTMLCanvasElement;
@@ -19,18 +51,25 @@ export default function Canvas() {
 
     for (let x = 0; x < 28; x++) {
       for (let y = 0; y < 28; y++) {
-        const color = pixels[x + (y * 28)] * 255;
+        const color = pixels[x + y * 28] * 255;
         ctx.fillStyle = `rgb(${color}, ${color}, ${color})`;
         ctx.fillRect(x * 20, y * 20, 20, 20);
       }
+    }
+
+    if (!network) {
+      setupBackend(WASM)
+        .then(() => fetch("mnist.test.bin"))
+        .then((res) => res.arrayBuffer())
+        .then((buffer) => setNetwork(Sequential.load(new Uint8Array(buffer))));
     }
   }, [pixels]);
 
   return (
     <div class="flex flex-col gap-2 w-full items-center">
-    <div class="bg-gray-800 text-white rounded-full py-2 px-4 mb-4">
-      <p class="font-bold">Prediction: {prediction}</p>
-    </div>
+      <div class="bg-gray-800 text-white rounded-full py-2 px-4 mb-4">
+        <p class="font-bold">Prediction: {prediction}</p>
+      </div>
       <canvas
         id="canvas"
         class="border border-2 w-min"
@@ -55,21 +94,27 @@ export default function Canvas() {
           for (let dx = -size; dx < size; dx++) {
             for (let dy = -size; dy < size; dy++) {
               if (mode === "draw") {
-                newPixels[x + dx + ((y + dy) * 28)] -= Math.pow(
+                newPixels[x + dx + (y + dy) * 28] -= Math.pow(
                   strength,
-                  Math.abs(dx) + Math.abs(dy),
+                  Math.abs(dx) + Math.abs(dy)
                 );
               } else if (mode === "erase") {
-                newPixels[x + dx + ((y + dy) * 28)] += Math.pow(
+                newPixels[x + dx + (y + dy) * 28] += Math.pow(
                   strength,
-                  Math.abs(dx) + Math.abs(dy),
+                  Math.abs(dx) + Math.abs(dy)
                 );
               }
             }
           }
 
-          console.log(x, y);
           setPixels(newPixels);
+          if (network && !predicting) {
+            setPredicting(true);
+            network
+              .predict(normalize(tensor(pixels, [1, 28, 28])))
+              .then((output) => setPrediction(argmax(output)))
+              .then(() => setPredicting(false));
+          }
         }}
       />
       <div class="flex gap-2">
@@ -88,12 +133,6 @@ export default function Canvas() {
           }}
         >
           <IconEraser class="w-6 h-6" /> Erase
-        </button>
-        <button
-          type="button"
-          class="px-3 py-2 bg-gray-800 text-white rounded hover:bg-gray-700 flex gap-2"
-        >
-          <IconSend class="w-6 h-6" /> Predict
         </button>
       </div>
     </div>
